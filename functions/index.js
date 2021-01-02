2b02c8fefa966463f47419345aa2fd67b59d8da7
Triet Ong <trietjack@gmail.com>
2021-01-02 21:53:50 +1000

    comment out algolia search functionality because of firebase plan downgrade

--- functions/index.js
+++ functions/index.js
@@ -8,37 +8,37 @@ const algoliasearch = require('algoliasearch');
 //  response.send("Hello from Firebase!");
 // });
 
-const ALGOLIA_ID = functions.config().algolia.appid;
-const ALGOLIA_ADMIN_KEY = functions.config().algolia.adminkey;
-const ALGOLIA_INDEX_NAME = 'bibo_search';
+// const ALGOLIA_ID = functions.config().algolia.appid;
+// const ALGOLIA_ADMIN_KEY = functions.config().algolia.adminkey;
+// const ALGOLIA_INDEX_NAME = 'bibo_search';
 
-const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
-const index = client.initIndex(ALGOLIA_INDEX_NAME);
+// const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+// const index = client.initIndex(ALGOLIA_INDEX_NAME);
 
-const videoDoc = 'videos/{videoId}';
+// const videoDoc = 'videos/{videoId}';
 
-exports.onVideoCreated = functions.firestore
-  .document(videoDoc)
-  .onCreate((snap, context) => {
-    const video = snap.data();
-    video.objectID = context.params.videoId;
+// exports.onVideoCreated = functions.firestore
+//   .document(videoDoc)
+//   .onCreate((snap, context) => {
+//     const video = snap.data();
+//     video.objectID = context.params.videoId;
 
-    return index.saveObject(video);
-  });
+//     return index.saveObject(video);
+//   });
 
-exports.onVideoUpdated = functions.firestore
-  .document(videoDoc)
-  .onUpdate((change, context) => {
-    const video = change.after.data();
-    video.objectID = context.params.videoId;
+// exports.onVideoUpdated = functions.firestore
+//   .document(videoDoc)
+//   .onUpdate((change, context) => {
+//     const video = change.after.data();
+//     video.objectID = context.params.videoId;
 
-    return index.saveObject(video);
-  });
+//     return index.saveObject(video);
+//   });
 
-exports.onVideoDeleted = functions.firestore
-  .document(videoDoc)
-  .onDelete((snap, context) => {
-    const objectID = context.params.videoId;
+// exports.onVideoDeleted = functions.firestore
+//   .document(videoDoc)
+//   .onDelete((snap, context) => {
+//     const objectID = context.params.videoId;
 
-    return index.deleteObject(objectID);
-  });
+//     return index.deleteObject(objectID);
+//   });
